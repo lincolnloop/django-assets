362ba4ab6883a391759b4deea8d051b371ee8bbe
Brian Luft <brian@lincolnloop.com>
2013-12-23 14:39:17 -0800

    Use staticfiles for finding files regardless of DEBUG

--- django_assets/env.py
+++ django_assets/env.py
@@ -112,8 +112,7 @@ class DjangoResolver(Resolver):
 
     @property
     def use_staticfiles(self):
-        return settings.DEBUG and \
-            'django.contrib.staticfiles' in settings.INSTALLED_APPS
+        return 'django.contrib.staticfiles' in settings.INSTALLED_APPS
 
     def glob_staticfiles(self, item):
         # The staticfiles finder system can't do globs, but we can
